34d985dbd7c092b23cc3ed169cc23e69c44b727f
javabear3388 <jiler3388@gmail.com>
2022-08-02 20:35:18 -0400

    Some practice

--- oop-review/src/main/java/reviewInheritance/Main.java
+++ oop-review/src/main/java/reviewInheritance/Main.java
@@ -3,6 +3,25 @@ package reviewInheritance;
 public class Main {
 
     public static void main(String[] args) {
-        
+
+        printThis("A Method!");
+        new Main().method1();
+
+        GoodStudent nana = new GoodStudent("nana", 15);
+        nana.printStudent();
+    }
+
+    public void method1(){
+        System.out.println("===inside method 1===");
+        System.out.println(check());
+        System.out.println("===inside method 1===");
+    }
+
+    private static void printThis(String s){
+        System.out.println(s);
+    }
+
+    private boolean check() {
+        return false;
     }
 }
