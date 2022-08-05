2301f2e9cff13608880699fc1f0a6d9a23131bc2
javabear3388 <jiler3388@gmail.com>
2022-08-05 19:47:53 -0400

    Static blocks demo

---
+++ oop-review/src/main/java/reviewInheritance/StaticDemo.java
@@ -0,0 +1,23 @@
+package reviewInheritance;
+
+public class StaticDemo {
+
+    public static void main(String[] args) {
+        System.out.println(Order.result +"");
+        System.out.println(Order.result +"");
+
+        new Order();
+        new Order();
+
+        System.out.println(Order.result +"");
+
+    }
+}
+
+class Order{
+    static String result = "";
+
+    {result += "c";}
+    static { result += "u";}
+    static { result += "r";}
+}
