e62aa3bc63b97b50048078f61f13a0365ca07fd5
javabear3388 <jiler3388@gmail.com>
2022-08-05 19:51:29 -0400

    Lombok

---
+++ lambda/src/main/java/Student.java
@@ -0,0 +1,24 @@
+import lombok.*;
+
+
+@Data
+//@NoArgsConstructor
+@AllArgsConstructor
+//@Getter
+//@Setter
+//@ToString
+//@EqualsAndHashCode
+//@RequiredArgsConstructor //for final variable
+public class Student {
+
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String email;
+    private String password;
+    private String confirmPassword;
+//    private final int age;
+
+
+
+}
