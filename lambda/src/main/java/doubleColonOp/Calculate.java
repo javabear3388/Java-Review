9c9016b6cfe24ad2c3e36a5b74e9a48dbfa2211f
javabear3388 <jiler3388@gmail.com>
2022-08-06 09:10:30 -0400

    Functional interface can also have default and static methods

--- lambda/src/main/java/doubleColonOp/Calculate.java
+++ lambda/src/main/java/doubleColonOp/Calculate.java
@@ -1,5 +1,8 @@
 package doubleColonOp;
-
+@FunctionalInterface
 public interface Calculate {
     void calculate(int x, int y);
+    default void m1(){
+        System.out.println("123");
+    };
 }
