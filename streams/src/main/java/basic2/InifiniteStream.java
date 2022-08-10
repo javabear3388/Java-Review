a94f0991f3b2c544c541e2dc75bc18c78667bdcb
javabear3388 <jiler3388@gmail.com>
2022-08-10 17:32:09 -0400

    Infinite stream

---
+++ streams/src/main/java/basic2/InifiniteStream.java
@@ -0,0 +1,14 @@
+package basic2;
+
+import java.util.stream.Stream;
+
+public class InifiniteStream {
+    public static void main(String[] args) {
+
+        Stream.iterate(0, n->n+2)    //Use limit
+                .limit(5)
+                .forEach(System.out::println);
+
+
+    }
+}
