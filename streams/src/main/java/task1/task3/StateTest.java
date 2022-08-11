d1d3a631bfc383c771fd16f9eb0e51f1fb52e358
javabear3388 <jiler3388@gmail.com>
2022-08-10 20:32:30 -0400

    Stream practice - State and City

---
+++ streams/src/main/java/task1/task3/StateTest.java
@@ -0,0 +1,41 @@
+package task1.task3;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StateTest {
+
+    public static void main(String[] args) {
+
+        State va = new State();
+        va.addCity("McLean");
+        va.addCity("Arlington");
+        va.addCity("Fairfax");
+        va.addCity("FallsChurch");
+
+        State tx= new State();
+        tx.addCity("Dallas");
+        tx.addCity("Plano");
+        tx.addCity("Austin");
+        tx.addCity("Houston");
+
+        List<State> states = Arrays.asList(va, tx);
+
+        states.stream()
+                .map(s -> s.getCity())
+                .forEach(System.out::println);
+        // ==> returns Stream<List<String>>
+
+        states.stream()
+                .map(state -> state.getCity())
+                .flatMap(List::stream)
+                .forEach(System.out::println);
+
+
+
+
+
+    }
+
+
+}
